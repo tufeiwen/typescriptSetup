8c86d8a52d0ebc3514147b4bc2a9610463a4f90a
tufei <wenxngood@yahoo.com>
2016-09-25 19:34:37 -0400

    typescript vs code setup init

---
+++ app.ts
@@ -0,0 +1,7 @@
+class HelloTypeScript{
+    constructor(public message:string){
+
+    }
+}
+var hello=new HelloTypeScript("hi, serena.");
+console.log(hello);
---
+++ jsScripts/app.js
@@ -0,0 +1,9 @@
+var HelloTypeScript = (function () {
+    function HelloTypeScript(message) {
+        this.message = message;
+    }
+    return HelloTypeScript;
+}());
+var hello = new HelloTypeScript("hi, serena.");
+console.log(hello);
+//# sourceMappingURL=app.js.map
